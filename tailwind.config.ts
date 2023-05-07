846fcec5352d4daaf1f8c013cd92658977a34276
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-07 09:58:07 -0400

    Upgrade Tailwind config to TS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -8,3 +10,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
