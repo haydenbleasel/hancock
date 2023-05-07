a1124de32d6abd10fb9b17ab149fe1af61114af5
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-07 10:46:13 -0400

    Add Gravity, remove react-syntax-highlighter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@beskar-labs/gravity/**/*.js',
   ],
   theme: {
     extend: {},
