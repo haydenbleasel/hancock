dcd99874abf9d2bb793fef3d308d20589e6dad9e
Hayden Bleasel <hello@haydenbleasel.com>
2023-12-08 09:47:08 -0500

    Upgrade Harmony

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
-import harmony from '@beskar-labs/harmony';
+import harmony from 'eslint-config-harmony';
+
+harmony.forEach((config) => {
+  if (config.ignores) {
+    config.ignores.push('./components/ui/**/*');
+  } else {
+    config.ignores = ['./components/ui/**/*'];
+  }
+});
 
 export default harmony;
